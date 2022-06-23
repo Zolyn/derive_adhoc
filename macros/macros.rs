ccbddf3467549c2182f3ea7c531fda151b2894f9
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-06-23 14:44:35 +0100

    W

--- macros/macros.rs
+++ macros/macros.rs
@@ -1,1 +1,8 @@
 //!
+
+#[proc_macro]
+pub fn derive_adhoc_expand(input: proc_macro::TokenStream)
+                           -> proc_macro::TokenStream {
+    eprintln!("{:?}", &input);
+    input
+}
--- test/second.rs
+++ test/second.rs
@@ -1,4 +1,35 @@
 
+macro_rules! derive_adhoc_apply_ChannelsParams {
+ { $($template:tt)* } => {
+   derive_adhoc_expand!{ 
+     {
+         #[derive(Debug, Educe, Clone, Eq, PartialEq)]
+         #[educe(Default)]
+          pub struct ChannelsParams {
+            $(
+              $( #[doc $($doc_attr)*] )*
+              $( #[$other_attr] )*
+              pub(crate) $field: $ty,
+            )*
+          }
+     }
+     $($template)*
+   }
+ }
+}
+
+derive_adhoc_apply_ChannelsParams!{
+    #[derive_adhoc(ChannelsParams)]
+    #[derive(Debug, Default, Clone, Eq, PartialEq)]
+    pub struct ChannelsParamsUpdates {
+    $(
+	/// New value, if it has changed.
+	pub(crate) $field: Option<$ty>,
+    )*
+    }
+}
+
+
 #[test]
 fn second(){
 }
