90223b50335b92cf85880e098281e7a7105664fc
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-06-27 18:53:00 +0100

    add note about lib crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
 //!
+
+// This is the "library" crate.
+//
+// It is conventional for proc macro crates to be wrapped up
+// in a library crate, which reexport the proc macros, mostly because
+// a proc macro crate cannot contain anything else.
+//
+// Currently our plans do not appear to include any items other than
+// proc macros.  But we shouldn't foreclose that.  So for now
+// this library crate ought to declare a dependency on the macro crate
+// and reexport the macros.
+//
+// Also this crate is where the docs live, right here as crate-level
+// docs.
