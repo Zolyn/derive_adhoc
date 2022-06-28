2c030613c81de8ed14873c1825d228ce517c531b
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-06-28 15:13:48 +0100

    parse with syn

--- macros/expand.rs
+++ macros/expand.rs
@@ -1,6 +1,23 @@
 
 use crate::prelude::*;
 
+struct ExpansionInput {
+    #[allow(dead_code)]
+    brace_token: token::Brace,
+    driver: syn::DeriveInput,
+    template: TokenStream,
+}
+
+impl Parse for ExpansionInput {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let driver;
+        let brace_token = braced!(driver in input);
+        let driver = driver.parse()?;
+        let template = input.parse()?;
+        Ok(ExpansionInput { brace_token, driver, template })
+    }
+}
+
 // This should implement the actual template engine
 //
 // In my design, the input contains, firstly, literally the definition
@@ -19,29 +36,15 @@ use crate::prelude::*;
 // with the original struct ident.
 pub fn derive_adhoc_expand_func_macro(input: TokenStream)
                                       -> syn::Result<TokenStream> {
-    let input: proc_macro2::TokenStream = input.into();
-    let mut input = input.into_iter();
-
-    let derive_input = input.next().unwrap();
-    let derive_input = match derive_input {
-        TokenTree::Group(g) => g,
-        _ => panic!(),
-    };
-    match derive_input.delimiter() {
-        Delimiter::Brace => { },
-        _ => panic!(),
-    }
-    // Should parse it into a syn::DeriveInput, really
-    let ident = derive_input.stream().into_iter().find_map(|tt| match tt {
-        TokenTree::Ident(i) if i.to_string().starts_with('C')  => Some(i),
-        _ => None,
-    }).unwrap_or_else(|| panic!());
+    let input: ExpansionInput = syn::parse2(input)?;
+    let ident = &input.driver.ident;
     dbg!(&ident);
 
     // maybe we should be using syn::buffer::TokenBuffer ?
     // or Vec<TokenTree>, which we parse into a tree of our own full
     // of [TokenTree] ?
     let mut output = TokenStream::new();
+    let mut input = input.template.into_iter();
     while let Some(token) = input.next() {
         match token {
             TokenTree::Punct(p) if p.as_char() == '$' => {
--- macros/prelude.rs
+++ macros/prelude.rs
@@ -3,5 +3,6 @@
 pub use proc_macro2::{Delimiter, TokenStream, TokenTree};
 pub use quote::{format_ident, quote, ToTokens};
 pub use syn::Token;
+pub use syn::braced;
 pub use syn::parse::{Parse, ParseStream};
-pub use syn::parse_macro_input;
+pub use syn::{parse_macro_input, token};
