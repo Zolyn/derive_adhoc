ad309c85f3265095ca83e36eee866645bc36eed2
Nick Mathewson <nickm@torproject.org>
2022-06-28 11:52:46 -0400

    Different way to do enums and structs together

--- playground/hash2.rs
+++ playground/hash2.rs
@@ -13,8 +13,19 @@ enum Enum {
     Case3(u8, u8),
 }
 
+#[derive(Adhoc)]
+#[derive_adhoc(MyHash)]
+struct Basic {
+    a: u8,
+    b : u8
+}
+
+#[derive(Adhoc)]
+#[derive_adhoc(MyHash)]
+struct Tuple(u8, u8);
+
 define_derive_adhoc!{
-    MyHash on enum = { // [1]
+    MyHash on any = { // [1]
         impl Hash for $self
             where $($( ${when not(attr(hash::skip))}
                      $ty : Hash + ))* // [2]
@@ -23,7 +34,8 @@ define_derive_adhoc!{
                 match self {
                     $(
                         $pat => { // [3]
-                            stringify!($variant).hash(state); // [4]
+                            $(${when enum} // [3a]
+                                stringify!($variant).hash(state); )// [4]
                             $(${when not(attr(hash::skip))}
                               $patfield.hash(state); // [5]
                             )
@@ -35,11 +47,16 @@ define_derive_adhoc!{
     }
 }
 
-// [1] I've written this to apply to enums only.
+// [1] Here "on any" means that we intend this to work on enums and
+//    structs.  Structs in this context are implicitly treated close
+//    to single-variant enums.
 // [2] Note that with an enum, we have to nest two levels deep to see the
 //     types.
-// [3] $pat is a pattern that deconstructs the current variant.
-// [4] $variant is the current variant, without any fields.
+// [3] $pat is a pattern that deconstructs the current variant.  If the
+//    current type is a struct, it deconstruct the struct.
+// [3a] enum is an expression that is true iff the currrent type is an enum.
+// [4] $variant is the current variant name, without any fields.  If the
+//     current type is not an enum, it fails to expand.
 // [5] $patfield is a field within $pat.
 
 // This expands to:
@@ -55,7 +72,7 @@ impl Hash for Enum
             Case2 { a, unhashed } => {
                 "Case2".hash(state);
                 a.hash(state);
-                unhashed.hash(state);
+                // XXX do we care about "unhashed is unused" warnings?
             }
             Case3(_0, _1) => {
                 "Case3".hash(state);
@@ -65,3 +82,24 @@ impl Hash for Enum
         }
     }
 }
+
+impl Hash for Basic
+    where u8: Hash + u8: Hash +
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        match self {
+            Basic { a, b } => { a.hash(state); b.hash(state); }
+        }
+    }
+}
+
+impl Hash for Tuple
+    where u8: Hash + u8: Hash +
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        match self {
+            Tuple(_0, _1) => { _0.hash(state); _1.hash(state); }
+        }
+    }
+}
+
