199c248771e9defb1349bf5561240960a0ca48a9
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-06-30 19:02:43 +0100

    make hash compile

--- src/bin/hash.rs
+++ src/bin/hash.rs
@@ -2,7 +2,32 @@
 // imagine what parameters look like, and how a "skip" attribute might
 // look, and how a "hash_with" attrbite might look.
 
+//#![feature(trace_macros)]
+//trace_macros!(true);
+
+use derive_adhoc_macros::{derive_adhoc, derive_adhoc_expand, Adhoc};
+use derive_adhoc_macros::{define_derive_adhoc};
+
+
+use std::hash::{Hash, Hasher};
+use std::fmt::Debug;
+
+define_derive_adhoc!{
+    MyHash /*for struct*/ =  // [1]
+
+    impl Hash for $ttype
+    where $( ${when not(fattr(hash(skip)))}
+             $ftype : Hash , )
+    {
+        fn hash<H : Hasher>(&self, state: &mut H) {
+            $( ${when not(fattr(hash::skip))}
+               self.$fname.hash(state); )
+        }
+    }
+}
+
 #[derive(Adhoc)]
+//#[derive_adhoc(MyHash)]
 #[derive_adhoc(MyHash)]
 struct DataType {
     foo: u8,
@@ -10,6 +35,8 @@ struct DataType {
     bar: Vec<String>,
 }
 
+
+/*
 #[derive(Adhoc)]
 #[derive_adhoc(MyHash)]
 struct Pair<S,T:Debug>
@@ -18,30 +45,18 @@ struct Pair<S,T:Debug>
     first: S,
     second: T,
 }
+*/
 
 #[derive(Adhoc)]
 #[derive_adhoc(MyHash)]
 struct IntPair( usize, usize );
-
-define_derive_adhoc!{
-    MyHash /*for struct*/ =  // [1]
-
-    impl Hash for $ttype
-    where $( ${when not(fattr(hash(skip)))}
-             $ftype : Hash + )*
-    {
-        fn hash<H : Hasher>(&self, state: &mut H) {
-            $( ${when not(fattr(hash::skip))}
-               self.$fname.hash(state); )*
-        }
-    }
-}
 
 // [1] The "for struct" syntax here means that only structs are supported.
 
 
 // This should expand to:
 
+/*
 impl Hash for DataType where u8 : Hash {
     fn hash<H : Hasher>(&self, state: &mut H) {
         self.foo.hash(state);
@@ -49,7 +64,7 @@ impl Hash for DataType where u8 : Hash {
 }
 
 impl<S,T> Hash for Pair<S,T> where
-    S: Hash + T: Hash + S: Debug + T: Debug // [3]
+    S: Hash, T: Hash, S: Debug, T: Debug // [3]
 {
     fn hash<H : Hasher>(&self, state: &mut H) {
         self.first.hash(state);
@@ -58,15 +73,25 @@ impl<S,T> Hash for Pair<S,T> where
 }
 
 impl Hash for IntPair
-    where usize: Hash + usize: Hash
+    where usize: Hash, usize: Hash
 {
     fn hash<H : Hasher>(&self, state: &mut H) {
         self.0.hash(state);
         self.1.hash(state);
     }
 }
+*/
 
 // [3] Note that the "impl..where .. {}" generation here is completely magical.
 //    It needs to add the <S,T> after the impl,
 //    and it needs to add S:Debug and T:Debug.
 
+fn main() {
+    let v = DataType {
+        foo: 23,
+        bar: vec!["hi".into()],
+    };
+    let mut hasher = std::collections::hash_map::DefaultHasher::new();
+    v.hash(&mut hasher);
+    println!("{:x}", hasher.finish());
+}
