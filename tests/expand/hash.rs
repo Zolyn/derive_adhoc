71e399dda0f395c58454d26d862690f86a78e199
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-01-16 14:49:39 +0000

    Swap link direction between tests/expand/ and tests/
    We're going to get rid of the files in tests/ and this makes the next
    diff much more comprehensible.

--- tests/expand/hash.rs
+++ tests/expand/hash.rs
@@ -1,1 +1,66 @@
-../hash.rs
+// Here's a simple syntax for the Hash attribute.  I'm using it to
+// imagine what parameters look like, and how a "skip" attribute might
+// look, and how a "hash_with" attrbite might look.
+
+//#![feature(trace_macros)]
+//trace_macros!(true);
+
+use derive_adhoc::define_derive_adhoc;
+use derive_adhoc::{derive_adhoc, Adhoc};
+
+use std::fmt::Debug;
+use std::hash::{Hash, Hasher};
+
+define_derive_adhoc! {
+    MyHash /*for struct*/ =  // [1]
+
+    impl<$tgens> Hash for $ttype
+    where $twheres
+          $( ${when not(fmeta(hash(skip)))}
+             $ftype : Hash , )
+    {
+        fn hash<H : Hasher>(&self, state: &mut H) {
+            $( ${when not(fmeta(hash::skip))}
+               self.$fname.hash(state); )
+        }
+    }
+}
+
+#[derive(Adhoc)]
+#[derive_adhoc(MyHash)]
+struct DataType {
+    foo: u8,
+    #[adhoc(hash(skip))]
+    bar: Vec<String>,
+}
+
+#[derive(Adhoc)]
+#[derive_adhoc(MyHash)]
+struct Pair<S, T: Debug>
+where
+    S: Debug,
+{
+    first: S,
+    second: T,
+}
+
+#[derive(Adhoc)]
+#[derive_adhoc(MyHash)]
+struct IntPair(usize, usize);
+
+// [1] The "for struct" syntax here means that only structs are supported.
+
+fn main() {
+    let v = DataType {
+        foo: 23,
+        bar: vec!["hi".into()],
+    };
+    let mut hasher = std::collections::hash_map::DefaultHasher::new();
+    v.hash(&mut hasher);
+    println!("{:x}", hasher.finish());
+}
+
+#[test]
+fn test() {
+    main()
+}
--- tests/expand/second.rs
+++ tests/expand/second.rs
@@ -1,1 +1,70 @@
-../second.rs
+use derive_adhoc::{derive_adhoc, Adhoc};
+
+#[derive(Adhoc, Default)]
+/// Some docs
+pub struct ChannelsParams {
+    /// thing
+    ///
+    /// paragraph
+    #[allow(dead_code)]
+    padding_enable: bool,
+
+    #[allow(dead_code)]
+    padding_parameters: usize,
+}
+
+derive_adhoc! {
+    ChannelsParams:
+
+    #[derive(Debug, Default, Clone, Eq, PartialEq)]
+    pub struct ChannelsParamsUpdates {
+        $(
+            // ${fattrs doc[0]}
+            ${fattrs serde}
+            ///
+            /// New value, if it has changed.
+            //
+            // ${fattrs doc[+]}
+            pub(crate) $fname: Option<$ftype>,
+        )
+    }
+}
+
+derive_adhoc! {
+    ChannelsParams:
+
+    #[allow(dead_code)]
+    ${tattrs doc, serde}
+    struct ChannelsParamsDupliate {
+        $(
+            ${fattrs ! serde}
+            $fname: $ftype,
+        )
+    }
+}
+
+derive_adhoc! {
+    ChannelsParams:
+    type Wombat = $ tname;
+}
+
+type K = Wombat;
+
+fn main() {
+    let _: K = ChannelsParams::default();
+
+    derive_adhoc! {
+        ChannelsParams:
+        $(
+            println!("field name {:?}", stringify!($fname));
+        )
+    }
+
+    let u = ChannelsParamsUpdates::default();
+    println!("updates = {:?}", &u);
+}
+
+#[test]
+fn test() {
+    main()
+}
