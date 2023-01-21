30140819fd0d68b9c62e903a58c2d7062bde9714
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-01-21 22:04:25 +0000

    select1: Add a failing test

---
+++ tests/ui/badselect1.rs
@@ -0,0 +1,51 @@
+// Here's a simple syntax for the Hash attribute.  I'm using it to
+// imagine what parameters look like, and how a "skip" attribute might
+// look, and how a "hash_with" attrbite might look.
+
+//#![feature(trace_macros)]
+//trace_macros!(true);
+
+use derive_adhoc::{derive_adhoc, Adhoc};
+
+use std::fmt::Debug;
+
+#[derive(Adhoc, Default, Debug)]
+struct Both {
+    #[adhoc(left)]
+    a: usize,
+    #[adhoc(right)]
+    b: usize,
+    #[adhoc(right)]
+    #[adhoc(left)]
+    c: usize,
+}
+
+derive_adhoc!{
+    Both:
+    
+    #[derive(Default, Debug)]
+    struct Left {
+        $(
+            ${select1
+              fmeta(left) { $fname: $ftype, } else if
+              fmeta(right) { }
+            }
+        )
+    }
+    #[derive(Default, Debug)]
+    struct Right {
+        $(
+            ${select1
+              fmeta(sinister) { } else if
+              fmeta(right) { $fname: $ftype, }
+            }
+        )
+    }
+}
+
+fn main() {
+    assert_eq!(
+        format!("{:?}", &Left::default()),
+        "Left { a: 0 }",
+    )
+}
