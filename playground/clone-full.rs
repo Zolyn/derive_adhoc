0545a3b5db7971626ac56ec50fe512a65a5e1cdf
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-02-15 22:36:39 +0000

    clone-full demo: Inconsequential compile fixes

--- playground/clone-full.rs
+++ playground/clone-full.rs
@@ -4,6 +4,8 @@
 //
 // This gives a basic demonstration of how to handle an enum.
 
+use derive_adhoc::{define_derive_adhoc, Adhoc};
+
 define_derive_adhoc!{
     PreciseClone =
 
@@ -18,7 +20,7 @@ define_derive_adhoc!{
     // // I think it must start with $f... not $p... since it is per-field.
     // // It needs to expand to a prefix so we can bind to variables
     // // which won't clash with fixed locals / other parameters.
-    impl Clone for $ttype
+    impl<$tgens> Clone for $ttype
     // We don't need to $( $( ) ) (ie, twice); it automatically descends.
     where $( $ftype: Clone, )
           $twheres
@@ -39,7 +41,7 @@ struct Unit;
 
 #[derive(Adhoc)]
 #[derive_adhoc(PreciseClone)]
-struct Tuple<F>(F)
+struct Tuple<F>(F);
 
 #[derive(Adhoc)]
 #[derive_adhoc(PreciseClone)]
@@ -61,10 +63,10 @@ fn test<T: Clone>(value: &T) {
 }
 
 fn main() {
-    test(Unit);
-    test(Tuple(String::new()));
-    test(Struct { field: 42 });
-    test(Enum::Unit);
-    test(Enum::Tuple(String::new()));
-    test(Enum::Struct { field: 66 });
+    test(&Unit);
+    test(&Tuple(String::new()));
+    test(&Struct { field: 42 });
+    test(&Enum::<()>::Unit);
+    test(&Enum::Tuple(String::new()));
+    test(&Enum::Struct { field: 66 });
 }
