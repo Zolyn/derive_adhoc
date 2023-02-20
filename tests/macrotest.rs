001db9f114dda8c00ce8c634161b6907a5235504
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-02-20 12:31:47 +0000

    macrotest optimisation: Call macrotest only once, if we can

--- tests/macrotest.rs
+++ tests/macrotest.rs
@@ -3,7 +3,7 @@ use derive_adhoc_tests::*;
 #[cfg(feature = "macrotest")]
 #[test]
 pub fn macrotest_expand() {
-    for path in list_expand_test_paths() {
+    for path in list_expand_test_paths_for_macrotest() {
         macrotest::expand(path);
     }
 }
