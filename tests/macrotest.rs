8b9fdaa0c1ff8f7e4982cdb14f2c5a633ea45a17
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-03-05 17:37:09 +0000

    Test minimal features
    * Run trybuild and macrotest only with full features.
    * Enable --all-features when running the macrotests.
    * Have the test crate disable derive-adhoc's default features.
    * Enable full features in the primary tests - for all compilers
      (no change to the main pinned nightly, since that hss --all-features.)
    * Add new minfeatures tests, that enable only recent (if applicable).
    * Explain all this in comments in macrotest.rs and trybuild.rs.

--- tests/macrotest.rs
+++ tests/macrotest.rs
@@ -1,9 +1,19 @@
+#[allow(unused_imports)]
 use derive_adhoc_tests::*;
 
+// We don't run this unless the CI explicitly enables it.
+// That's done only for the "cargo-test" test,
+// ie our pinned nightly, with full features enabled.
+//
+// We don't do expansion match tests other than on our pinned compiler.
+//
+// We don't do these tests without all derive-adhoc features enabled.
+// So there is n testing of what feature controls what.
+// That's OK, we're not going to have very many features.
 #[cfg(feature = "macrotest")]
 #[test]
 pub fn macrotest_expand() {
     for path in list_expand_test_paths_for_macrotest() {
-        macrotest::expand(path);
+        macrotest::expand_args(path, ["--all-features"]);
     }
 }
--- tests/trybuild.rs
+++ tests/trybuild.rs
@@ -1,6 +1,16 @@
+#[allow(unused_imports)]
 use derive_adhoc_tests::*;
 
+// We don't run this unless the CI explicitly enables it.
+// That's done only for the tests with all d-a features enabled,
+// Ie, the ones that aren't -minfeatures.
+//
+// So there is n testing of what feature controls what.
+// That's OK, we're not going to have very many features.
+//
+// The basic tests in tests.rs still run of course.
 #[test]
+#[cfg(feature = "full")]
 pub fn run_pass_expand() {
     let t = trybuild::TestCases::new();
 
@@ -14,7 +24,13 @@ pub fn run_pass_expand() {
 //
 // Instead, put the file in tests/, with `#[test]` annotations,
 // and add a `mod` line to `tests/tests.rs`, as with `list_names.rs`.
-
+//
+// We don't run this unless the CI explicitly enables it.
+// That's one only for our main test with the pinned compiler,
+// and all d-a features enabled.
+//
+// We basically trust that the features-disabled fallback code
+// for handling the disabled feature is good enough.
 #[cfg(feature = "ui")]
 #[test]
 pub fn ui() {
