89e7f2e80ef3e573e8e51cee0e175382b7703390
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-05-31 19:17:58 +0100

    New $Xmeta arrangements: testing: Add a test for bad identifier
    The error message is quite poor: nothing points to the actual problem.
    We'll fix that next.

--- tests/ui/badmeta.rs
+++ tests/ui/badmeta.rs
@@ -2,12 +2,24 @@ use derive_adhoc::{derive_adhoc, Adhoc};
 
 #[derive(Adhoc)]
 #[adhoc(wrong = "{")]
+#[adhoc(dot = ".")]
 struct DataType {}
 
 derive_adhoc! {
     DataType:
 
     const K: () = ${tmeta(wrong)}
+}
+// Invoke derive_adhoc! a second time, because (empirically) the
+// attempt to parse "{" causes some kind of nonlocal exit:
+// syn::LitStr::parse is called but doesn't return.  And then, something
+// arranges to report only the *last* such error.  But we want to see
+// all the errors, so invoke the macro separately.
+
+derive_adhoc! {
+    DataType:
+
+    struct ${paste Bad ${tmeta(dot) as str}};
 }
 
 fn main() {}
