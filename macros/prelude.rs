4af50e039200da4fba757fdcc2cfba11e8842485
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-06-26 01:28:35 +0100

    Drop import of catch_unwind etc.
    We don't want to be catching panics any more, as discussed in
    IdentAny::try_from_str's code comment.

--- macros/prelude.rs
+++ macros/prelude.rs
@@ -12,7 +12,6 @@ pub use std::iter;
 pub use std::marker::PhantomData;
 pub use std::mem;
 pub use std::ops::Deref;
-pub use std::panic::{catch_unwind, AssertUnwindSafe};
 pub use std::str::FromStr;
 pub use std::thread::panicking;
 
