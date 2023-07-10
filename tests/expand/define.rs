6af36f6e1fc95743f834558576eb61340e2a26d9
Ian Jackson <ijackson@chiark.greenend.org.uk>
2023-07-10 09:40:38 +0100

    definitions: tests: add a define.rs test case (2)

--- tests/expand/define.rs
+++ tests/expand/define.rs
@@ -41,10 +41,79 @@ derive_adhoc! {
     ];
 }
 
+#[derive(Adhoc)]
+enum T<'g> {
+    /// Expected value
+    E(i32),
+    /// Generated values (all must match corresponding `E`
+    G(&'g [i32]),
+}
+
+derive_adhoc! {
+    S expect items:
+
+    ${define CASE {
+        T::G(&[ 900 + $N ]),
+        T::G(&[ 100 + $N ${if not(C) { + 40 }},
+                ${if C { 100 + $N } else { 140 + $N }} ]),
+    }}
+
+    /// Interleaved expected and generated
+    const T_DATA: &[T] = &[
+
+        ${define N 1}
+        ${defcond C true}
+        ${CASE}
+        T::E(901), T::E(101),
+
+        ${define N 2}
+        ${defcond C false}
+        ${CASE}
+        T::E(902), T::E(142),
+
+        ${for fields {
+            ${define N 3}
+            ${defcond C true}
+            ${CASE}
+            T::E(903), T::E(103),
+        }}
+
+    ];
+}
+
+derive_adhoc! {
+    T expect items:
+
+    fn t_compare() {
+      $(
+        let mut ${snake_case $vname} = T_DATA
+            .iter()
+            .filter_map(|t| match t {
+                $vpat => Some($( $fpatname )),
+                _ => None,
+            });
+      )
+        // zip_longest without itertools
+        while let Some(e) = e.next() {
+            let g = g.next().unwrap();
+            for g in *g {
+                if e != g {
+                    std::process::abort();
+                }
+            }
+        }
+        if e.next().is_some() {
+            std::process::abort();
+        }
+    }
+}
+
 fn main() {
     for (exp, got) in L {
         if exp != got {
             std::process::abort()
         }
     }
+
+    t_compare();
 }
